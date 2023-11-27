fdcf5780a50bd7e0510ba7091971b1b7a09fd710
Michael Souter <souterm1@gmail.com>
2023-11-26 22:15:07 -0500

    Add project files.

---
+++ CRUD.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reports_Builder
+{
+    internal class CRUD
+    {
+        public static void GetDailyReport(string date)
+        {
+
+            try
+            {
+                string connectionString = @"Data Source=localhost;Initial Catalog=TextToDB;Integrated Security=True";
+                SqlConnection conn = new SqlConnection(connectionString);
+
+                SqlCommand cmd = new SqlCommand("sp_GetDailyReport", conn);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@date", date+'%');
+
+                conn.Open();
+                
+                using (SqlDataReader reader = cmd.ExecuteReader()) {
+                    int fieldCount = reader.FieldCount;
+                    while (reader.Read())
+                       {
+                        StringBuilder sb = new StringBuilder();
+
+                        for(int i =0; i < fieldCount; i++)
+                        {
+                            sb.Append(reader[i].ToString());
+                            if (i < fieldCount -1)
+                            {
+                                sb.Append(", ");
+                            }
+                        }
+
+                        Console.WriteLine(sb.ToString());
+                       }
+
+                    
+                }
+
+
+                conn.Close();
+            }
+            catch (SqlException exxp)
+            {
+                Console.WriteLine(exxp.Message);
+            }
+        }
+        
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using reports = Reports_Builder.CRUD;
+
+namespace reports_builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            reports.GetDailyReport("2023-01-03");
+        }
+    
+   
+    }
+
+    //EOC
+}
